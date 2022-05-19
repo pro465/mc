ee8959cbe1a5c9e517eb6d9d4312824a96f9d7cf
Pro465 <namitapro0987@gmail.com>
2022-05-19 20:21:17 +0530

    additional traps

--- src/trap.rs
+++ src/trap.rs
@@ -1,5 +1,8 @@
 use crate::vm::Vm;
-use std::io::{self, Write};
+use std::io::{self, Read, Write};
+
 pub const TRAP: &[fn(&mut Vm)] = &[
     |v| io::stdout().write_all(&[v.reg[0]]).unwrap(),
+    |v| io::stdin().read_exact(&mut v.reg[0..1]).unwrap(),
+    |_| io::stdout().flush().unwrap(),
 ];
