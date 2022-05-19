d4bcede03fb733ba61fb5a688d2b08c8bcc2767e
Pro465 <namitapro0987@gmail.com>
2022-05-19 21:09:33 +0530

    remove test in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use mc::vm::Vm;
+use std::env;
+use std::fs::File;
+use std::io::{ErrorKind, Read};
 
 fn main() {
     let mut mem = [0; 0x10000];
-    mem[0] = 0b0011_0000;
-    mem[1] = b'h';
-    mem[2] = 0b0010_0000;
+
+    for p in env::args().skip(1) {
+        let mut f = File::open(p).unwrap();
+        let mut origin = [0; 2];
+
+        f.read_exact(&mut origin).unwrap();
+
+        let origin = u16::from_be_bytes(origin) as usize;
+        if let Err(e) = f.read_exact(&mut mem[origin..]) {
+            if e.kind() != ErrorKind::UnexpectedEof {
+                Err(e).unwrap()
+            }
+        }
+    }
 
     let mut vm = Vm::new(mem);
 
     while !vm.halted() {
-       vm.step();
+        vm.step();
     }
 }
